8cf7631b95c4b23a605edbfe6e9706a32292a9da
Pa3JIy4HiK <paradoks6666@gmail.com>
2017-02-14 21:20:13 +0300

    test_array

--- ArraysUse.java
+++ ArraysUse.java
@@ -1,14 +1,14 @@
 public class ArraysUse {
     public static void main(String[] args) {
 
-        int[] array1 = {1, 1, 3};
+        int[] array1 = {1, 1,2,7,11,31, 3};
         int sum = 0;
         for (int number : array1)
             sum += number;
 
 
 
-     int [] array2 = {1,-2,5,110,13};
+     int [] array2 = {1,-2,5,110,66,73,13,54,13};
         int max = 0;
         for(int number : array2)
             if(number>0 && max<number)
@@ -17,20 +17,20 @@ public class ArraysUse {
 
 
 
-        int [] array3 = {1,2,3,11};
+        int [] array3 = {1,2,3,4,5,8,2,6,45,1};
         int product = 1;
         for(int number : array3)
             product *= number;
 
 
-        int [] array4 = {1,5,2,56};
+        int [] array4 = {1,56,32,5,42,56};
         int first = array4[0];
         int last = array4[array4.length - 1];
 
 
 
 
-        int [] array5 = {1,5,15,61,62};
+        int [] array5 = {1,5,15,54,23,12,61,62};
         int largest = array5[0];
         int secondLargest = array5[0];
         for (int number : array5)
